19617304521252dc5819d299382741cb6ad36719
hrbrd <hrbrd@hrbrd>
2020-01-27 13:34:02 +0100

    Initial commit

---
+++ src/main/java/pl/tuatara/demo/TestController.java
@@ -0,0 +1,15 @@
+package pl.tuatara.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+
+}
